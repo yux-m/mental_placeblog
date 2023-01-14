3c8362d96944256ba224f1ed90486fcf781c5eac
yux-m <yuxmeng@seas.upenn.edu>
2023-01-14 12:48:18 -0500

    Initial commit

---
+++ src/main/java/cit59x/hackathon/mental_placeblog/MentalPlaceblogApplication.java
@@ -0,0 +1,13 @@
+package cit59x.hackathon.mental_placeblog;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalPlaceblogApplication {
+
+    public static void main(String[] args) {
+        //TODO: run controller(s) here
+        System.out.println("It's working.");
+    }
+
+}
