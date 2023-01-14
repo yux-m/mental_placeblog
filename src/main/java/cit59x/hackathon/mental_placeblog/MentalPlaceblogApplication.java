a2fdfb679528e16b65be4b0589e64cefb88fe15d
yux-m <yuxmeng@seas.upenn.edu>
2023-01-14 13:19:25 -0500

    Simple API test with path "/test".

--- src/main/java/cit59x/hackathon/mental_placeblog/MentalPlaceblogApplication.java
+++ src/main/java/cit59x/hackathon/mental_placeblog/MentalPlaceblogApplication.java
@@ -1,13 +1,14 @@
 package cit59x.hackathon.mental_placeblog;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MentalPlaceblogApplication {
 
     public static void main(String[] args) {
-        //TODO: run controller(s) here
-        System.out.println("It's working.");
+        SpringApplication.run(MentalPlaceblogApplication.class, args);
     }
+
 
 }
---
+++ src/main/java/cit59x/hackathon/mental_placeblog/controller/TestController.java
@@ -0,0 +1,19 @@
+package cit59x.hackathon.mental_placeblog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    /**
+     * Initial test on localhost to make sure the app is working.
+     * Test passed: went to "localhost:8080/test" in browser and saw "Test Page".
+     *
+     * @return
+     */
+    @RequestMapping("/test")
+    public String Test() {
+        return "Test Page.";
+    }
+}
